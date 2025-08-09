7e7639af4703e7594fe67083bcbccbb788af90dc
Asilbek <asilturdiyev777@gmail.com>
2025-08-09 19:45:30 +0500

    Feat: Add Sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
 import {MainPageComponent} from './pages/main-page/main-page.component';
 import {RegisterComponent} from './auth/register/register.component';
+import {LayoutComponent} from './layout/layout.component';
+import {ChatbotComponent} from './pages/chatbot/chatbot.component';
 
 export const routes: Routes = [
-  { path: '', component: MainPageComponent },
+  {path: '', component: LayoutComponent, children:[
+    { path: '', component: MainPageComponent },
+    {path: 'chat-bot', component: ChatbotComponent},
+
+  ]},
   {path: 'sign-in', component: LoginComponent},
   {path: 'sign-up', component: RegisterComponent},
 ];
